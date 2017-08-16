2050013896acaf19921b7c0693c015582356f654
Claude Heintz <claudeheintzdesign@gmail.com>
2017-08-16 08:00:53 -0500

    update for changes in Arduino core

--- extras/modified_hal-uart/esp32-hal-uart.c
+++ extras/modified_hal-uart/esp32-hal-uart.c
@@ -105,7 +105,6 @@ static uart_t _uart_bus_array[3] = {
 
 static void IRAM_ATTR _uart_isr(void *arg)
 {
-	digitalWrite(5, HIGH);
     uint8_t i, c;
     BaseType_t xHigherPriorityTaskWoken;
     uart_t* uart;
@@ -113,7 +112,6 @@ static void IRAM_ATTR _uart_isr(void *arg)
 
     for(i=0;i<3;i++){
         uart = &_uart_bus_array[i];
-        
         uart->dev->int_clr.rxfifo_full = 1;
 		uart->dev->int_clr.frm_err = 1;
 		uart->dev->int_clr.rxfifo_tout = 1;
@@ -188,8 +186,6 @@ static void IRAM_ATTR _uart_isr(void *arg)
     if (xHigherPriorityTaskWoken) {
         portYIELD_FROM_ISR();
     }
-    
-    digitalWrite(5, LOW);
 }
 
 void uartEnableGlobalInterrupt()
@@ -299,7 +295,7 @@ uart_t* uartBegin(uint8_t uart_nr, uint32_t baudrate, uint32_t config, int8_t rx
     uart->dev->conf0.val = config;
     #define TWO_STOP_BITS_CONF 0x3
     #define ONE_STOP_BITS_CONF 0x1
-	
+
     if ( uart->dev->conf0.stop_bit_num == TWO_STOP_BITS_CONF) {
         uart->dev->conf0.stop_bit_num = ONE_STOP_BITS_CONF;
         uart->dev->rs485_conf.dl1_en = 1;
@@ -325,14 +321,17 @@ void uartEnd(uart_t* uart)
 
     UART_MUTEX_LOCK();
     if(uart->queue != NULL) {
+        uint8_t c;
+        while(xQueueReceive(uart->queue, &c, 0));
         vQueueDelete(uart->queue);
     }
 
+    uart->dev->conf0.val = 0;
+
+    UART_MUTEX_UNLOCK();
+
     uartDetachRx(uart);
     uartDetachTx(uart);
-
-    uart->dev->conf0.val = 0;
-    UART_MUTEX_UNLOCK();
 }
 
 uint32_t uartAvailable(uart_t* uart)
@@ -519,4 +518,3 @@ int log_printf(const char *format, ...)
     }
     return len;
 }
-
